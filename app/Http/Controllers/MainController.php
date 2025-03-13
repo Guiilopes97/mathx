8f3d2e70a8b6108fc485c0e7753437f62ed49f83
Guilherme Lopes de Oliveira <guilhermelopes97@outlook.com>
2025-03-13 20:37:14 -0300

    009

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,24 +15,33 @@ class MainController extends Controller
     public function genereateExercises(Request $request): View
     {
         // form validation
-        $request->validate([
-            'check_sum'=> 'required_without_all:check_subtraction,check_multiplication,check_division',
-            'check_subtraction'=> 'required_without_all:check_sum,check_multiplication,check_division',
-            'check_multiplication'=> 'required_without_all:check_sum,check_subtraction,check_division',
-            'check_division'=> 'required_without_all:check_sum,check_subtraction,check_multiplication',
+        $request->validate(
+            [
+                'check_sum' => 'required_without_all:check_subtraction,check_multiplication,check_division',
+                'check_subtraction' => 'required_without_all:check_sum,check_multiplication,check_division',
+                'check_multiplication' => 'required_without_all:check_sum,check_subtraction,check_division',
+                'check_division' => 'required_without_all:check_sum,check_subtraction,check_multiplication',
 
-            'number_one'=> 'required|integer|min:0|max:999|lt:number_two',
-            'number_two'=> 'required|integer|min:0|max:999',
-            'number_exercises'=> 'required|integer|min:5|max:50',
-        ]
-    );
+                'number_one' => 'required|integer|min:0|max:999|lt:number_two',
+                'number_two' => 'required|integer|min:0|max:999',
+                'number_exercises' => 'required|integer|min:5|max:50',
+            ]
+        );
 
         // get selected options
         $operations = [];
-        if ($request->check_sum) {$operations[] = 'sum';}
-        if ($request->check_subtraction) {$operations[] = 'subtraction';}
-        if ($request->check_multiplication) {$operations[] = 'multiplication';}
-        if ($request->check_division) {$operations[] = 'division';}
+        if ($request->check_sum) {
+            $operations[] = 'sum';
+        }
+        if ($request->check_subtraction) {
+            $operations[] = 'subtraction';
+        }
+        if ($request->check_multiplication) {
+            $operations[] = 'multiplication';
+        }
+        if ($request->check_division) {
+            $operations[] = 'division';
+        }
 
         // get numbers (min and max)
         $min = $request->number_one;
@@ -44,53 +53,13 @@ class MainController extends Controller
         // generate exercises
         $exercises = [];
         for ($i = 1; $i <= $number_exercises; $i++) {
-
-            $operation = $operations[array_rand($operations)];
-            $number1 = rand($min, $max);
-            $number2 = rand($min, $max);
-
-            $exercise = '';
-            $solution = '';
-
-            switch ($operation) {
-                case 'sum':
-                    $exercise = "$number1 + $number2 =";
-                    $solution = $number1 + $number2;
-                    break;
-                case 'subtraction':
-                    $exercise = "$number1 - $number2 =";
-                    $solution = $number1 - $number2;
-                    break;
-                case 'multiplication':
-                    $exercise = "$number1 x $number2 =";
-                    $solution = $number1 * $number2;
-                    break;
-                case 'division':
-
-                    // avoid division by zero
-                    if ($number2 == 0) {
-                        $number2 = 1;
-                    }
-
-                    $exercise = "$number1 / $number2 =";
-                    $solution = $number1 / $number2;
-                    break;
-            }
-
-            // if solution is a float, round it to 2 decimal places
-            if (is_float($solution)) {
-                $solution = round($solution, 2);
-            }
-
-            $exercises[] = [
-                "operation"=> $operation,
-                'exercise_number' => $i,
-                'exercise' => $exercise,
-                'solution' => "$exercise $solution",
-            ];
-
+            $exercises[] = $this->generateExercises($i, $operations, $min, $max);
         }
 
+        // place exercises in session
+        // $request->session()->put('exercises', $exercises);
+        // ou
+        session(['exercises' => $exercises]);
 
         return view('operations', ['exercises' => $exercises]);
     }
@@ -103,5 +72,52 @@ class MainController extends Controller
     public function exportExercises()
     {
         echo "exportar exercício para um arquivo de texto";
+    }
+
+    private function generateExercises($i, $operations, $min, $max, ): array
+    {
+        $operation = $operations[array_rand($operations)];
+        $number1 = rand($min, $max);
+        $number2 = rand($min, $max);
+
+        $exercise = '';
+        $solution = '';
+
+        switch ($operation) {
+            case 'sum':
+                $exercise = "$number1 + $number2 =";
+                $solution = $number1 + $number2;
+                break;
+            case 'subtraction':
+                $exercise = "$number1 - $number2 =";
+                $solution = $number1 - $number2;
+                break;
+            case 'multiplication':
+                $exercise = "$number1 x $number2 =";
+                $solution = $number1 * $number2;
+                break;
+            case 'division':
+
+                // avoid division by zero
+                if ($number2 == 0) {
+                    $number2 = 1;
+                }
+
+                $exercise = "$number1 / $number2 =";
+                $solution = $number1 / $number2;
+                break;
+        }
+
+        // if solution is a float, round it to 2 decimal places
+        if (is_float($solution)) {
+            $solution = round($solution, 2);
+        }
+
+        return [
+            'exercise_number' => $i,
+            "operation" => $operation,
+            'exercise' => $exercise,
+            'solution' => "$exercise $solution",
+        ];
     }
 }
